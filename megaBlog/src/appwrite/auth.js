ce11fb291a4815daf6f5d1a8f0c25da76507995c
amrit raj <amritrajsingh179@gmail.com>
2025-08-23 10:57:12 +0530

    mega-blog

---
+++ megaBlog/src/appwrite/auth.js
@@ -0,0 +1,55 @@
+import config from "../config/config";
+import {Client,Account,ID} from "appwrite";
+
+
+export class AuthService{
+    client= new Client();
+    account;
+
+    constructor(){
+        this.client
+        .setEndpoint(config.appwriteUrl)
+        .setProject(config.appwriteProjectId);
+        this.account = new Account(this.client)
+    }
+    async createAccount({email,password,name}){
+        try {
+           const userAccount =  await this.account.client(ID.unique(),email,password,name)
+           if(userAccount) {
+            if(userAccount){
+                return this.login({email,password})
+            }
+           }
+           else{
+            return userAccount
+           }
+        } catch (error) {
+            throw error;
+        }
+    }
+    async login ({email,password}){
+        try {
+            await this.account.createEmailPasswordSession(email,password)
+        } catch (error) {
+            throw error
+        }
+    }
+    async getCurrentUser () {
+        try {
+            await this.account.get();
+        } catch (error) {
+            console.log("apperite service :: getCurrentUser :: error", error);
+        }
+        return null;
+    }
+    async logout(){
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+             console.log("apperite service :: logout :: error", error);
+        }
+    }
+}
+
+const authservice = new AuthService()
+export default authservice;
